2ac0b3b55896d4f4ad7f696685457ebdfa39df84
aasyhadi <adiproojambi@gmail.com>
2026-06-24 15:47:38 +0700

    fix workflow ci mlflow project

--- Monitoring dan Logging/3.prometheus_exporter.py
+++ Monitoring dan Logging/3.prometheus_exporter.py
@@ -6,53 +6,30 @@ from prometheus_client import Counter, Histogram, Gauge, generate_latest, CONTEN
 
 app = Flask(__name__)
 
-REQUEST_COUNT = Counter(
-    "http_requests_total",
-    "Total HTTP Requests"
-)
-
-PREDICTION_COUNT = Counter(
-    "prediction_requests_total",
-    "Total prediction requests"
-)
-
-REQUEST_LATENCY = Histogram(
-    "prediction_latency_seconds",
-    "Prediction latency seconds"
-)
-
-LAST_PREDICTION = Gauge(
-    "last_prediction_value",
-    "Last prediction result"
-)
-
-CPU_USAGE = Gauge(
-    "system_cpu_usage",
-    "CPU Usage Percentage"
-)
-
-RAM_USAGE = Gauge(
-    "system_ram_usage",
-    "RAM Usage Percentage"
-)
+HTTP_REQUESTS_TOTAL = Counter("http_requests_total", "Total HTTP Requests")
+PREDICTION_REQUESTS_TOTAL = Counter("prediction_requests_total", "Total Prediction Requests")
+PREDICTION_LATENCY = Histogram("prediction_latency_seconds", "Prediction Latency Seconds")
+LAST_PREDICTION_VALUE = Gauge("last_prediction_value", "Last Prediction Result")
+SYSTEM_CPU_USAGE = Gauge("system_cpu_usage", "System CPU Usage Percentage")
+SYSTEM_RAM_USAGE = Gauge("system_ram_usage", "System RAM Usage Percentage")
 
 
 @app.route("/", methods=["GET"])
 def home():
-    REQUEST_COUNT.inc()
+    HTTP_REQUESTS_TOTAL.inc()
     return jsonify({"status": "running"})
 
 
 @app.route("/health", methods=["GET"])
 def health():
-    REQUEST_COUNT.inc()
+    HTTP_REQUESTS_TOTAL.inc()
     return jsonify({"status": "healthy"})
 
 
 @app.route("/metrics", methods=["GET"])
 def metrics():
-    CPU_USAGE.set(psutil.cpu_percent(interval=1))
-    RAM_USAGE.set(psutil.virtual_memory().percent)
+    SYSTEM_CPU_USAGE.set(psutil.cpu_percent(interval=1))
+    SYSTEM_RAM_USAGE.set(psutil.virtual_memory().percent)
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
@@ -60,25 +37,21 @@ def metrics():
 def predict():
     start_time = time.time()
 
-    REQUEST_COUNT.inc()
-    PREDICTION_COUNT.inc()
+    HTTP_REQUESTS_TOTAL.inc()
+    PREDICTION_REQUESTS_TOTAL.inc()
 
     api_url = "http://127.0.0.1:5005/invocations"
     data = request.get_json()
 
     try:
         response = requests.post(api_url, json=data)
-        duration = time.time() - start_time
-
-        REQUEST_LATENCY.observe(duration)
+        latency = time.time() - start_time
+        PREDICTION_LATENCY.observe(latency)
 
         result = response.json()
 
-        try:
-            prediction = result["predictions"][0]
-            LAST_PREDICTION.set(float(prediction))
-        except Exception:
-            LAST_PREDICTION.set(0)
+        prediction = result["predictions"][0]
+        LAST_PREDICTION_VALUE.set(float(prediction))
 
         return jsonify(result)
 
